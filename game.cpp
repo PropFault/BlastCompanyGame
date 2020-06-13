8315b8eeac5464966ac400901f9f52b68028982a
PropFaultVM <propfault@example.com>
2020-06-13 16:59:32 +0200

    Added input test code

--- game.cpp
+++ game.cpp
@@ -9,6 +9,7 @@
 #include "../BlastCompany/context.h"
 #include <inputevent.h>
 #include <inputaction.h>
+#include "../Box2DComponent/dynamicbodycomponent.h"
 namespace game {
 
     class MainGame : public Game{
@@ -18,6 +19,7 @@ namespace game {
         int c;
     public:
         void ballCallback(const boost::any &data, Component::CID animationCid){
+            std::cout<<data.type().hash_code()<<std::endl;
             CallbackActionComponent::CallbackEventArgs args(boost::any_cast<const CallbackActionComponent::CallbackEventArgs&>(data));
             if(args.state == CallbackActionComponent::CallbackState::START)
                 c++;
@@ -29,13 +31,21 @@ namespace game {
         void leftAxis(const boost::any &data, Component::CID inputComponent){
             InputEvent event(boost::any_cast<const InputEvent&>(data));
             std::cout<<"action: " << event.getAction() << " axisVal: " << event.getAxisValue() << " buttonVal: " << event.getButtonValue()<<std::endl;
+            std::cout<<"SIZ:" << event.getPackagedComponents().size()<<std::endl;
+            DynamicBodyComponent* dynBod = dynamic_cast<DynamicBodyComponent*>(event.getPackagedComponents().at(0));
+            b2Body *body = dynBod->getBody();
+            if(event.getAction() == InputAction::STICK_L_X)
+                body->ApplyForceToCenter(b2Vec2(event.getAxisValue()/10000.0,0), true);
+            else if(event.getAction() == InputAction::STICK_L_Y)
+                body->ApplyForceToCenter(b2Vec2(0,-event.getAxisValue()/10000.0), true);
+
         }
         void start(Context& context) override{
             std::cout<<"START"<<std::endl;
             this->context = &context;
             c = 0;
             context.getFuncMan().registerFunction("ballCallback", std::bind(&MainGame::ballCallback, this, std::placeholders::_1, std::placeholders::_2));
-            context.getFuncMan().registerFunction("leftAxis", std::bind(&MainGame::ballCallback, this, std::placeholders::_1, std::placeholders::_2));
+            context.getFuncMan().registerFunction("leftAxis", std::bind(&MainGame::leftAxis, this, std::placeholders::_1, std::placeholders::_2));
 
             //CallbackActionComponent::registerCallback("ballCallback",  game::ballCallback);
             std::ifstream baseballFile("./game/entities/baseball.json");
